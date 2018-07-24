d679906d88c9495842bd87ad8b8179941d0a12ef
taojian <taojian@baidu,com>
2018-07-24 16:27:48 +0800

    daily shell

---
+++ basic/5.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+#if else
+:<<EOF
+if condition
+then
+    command1 
+    command2
+    ...
+    commandN 
+fi
+
+if condition
+then
+    command1 
+    command2
+    ...
+    commandN 
+fi
+
+if condition
+then
+    command1 
+    command2
+    ...
+    commandN
+else
+    command
+fi
+
+
+if condition1
+then
+    command1
+elif condition2 
+then 
+    command2
+else
+    commandN
+fi
+EOF
+a=10
+b=20
+if [ $a == $b ]
+then
+   echo "a 等于 b"
+elif [ $a -gt $b ]
+then
+   echo "a 大于 b"
+elif [ $a -lt $b ]
+then
+   echo "a 小于 b"
+else
+   echo "没有符合的条件"
+fi
+
+
+num1=$[2*3]
+num2=$[1+5]
+if test $[num1] -eq $[num2]
+then
+    echo '两个数字相等!'
+else
+    echo '两个数字不相等!'
+fi
+
+# for
+
+for str in 'This is a string'
+do
+    echo $str
+done
+
+
+
+for loop in 1 2 3 4 5
+do
+    echo "The value is: $loop"
+done
+
+
+# while
+int=1
+while(( $int<=5 ))
+do
+    echo $int
+    let "int++"
+done
+
+echo '按下 <CTRL-D> 退出'
+echo -n '输入你最喜欢的网站名: '
+while read FILM
+do
+    echo "是的！$FILM 是一个好网站"
+done
+
+# case
+
+echo '输入 1 到 4 之间的数字:'
+echo '你输入的数字为:'
+read aNum
+case $aNum in
+    1)  echo '你选择了 1'
+    ;;
+    2)  echo '你选择了 2'
+    ;;
+    3)  echo '你选择了 3'
+    ;;
+    4)  echo '你选择了 4'
+    ;;
+    *)  echo '你没有输入 1 到 4 之间的数字'
+    ;;
+esac
+
+#fun ()
+
+
+demoFun(){
+    echo "这是我的第一个 shell 函数!"
+}
+echo "-----函数开始执行-----"
+demoFun
+echo "-----函数执行完毕-----"
+
+
+funWithReturn(){
+    echo "这个函数会对输入的两个数字进行相加运算..."
+    echo "输入第一个数字: "
+    read aNum
+    echo "输入第二个数字: "
+    read anotherNum
+    echo "两个数字分别为 $aNum 和 $anotherNum !"
+    return $(($aNum+$anotherNum))
+}
+funWithReturn
+echo "输入的两个数字之和为 $? !"
+
+
